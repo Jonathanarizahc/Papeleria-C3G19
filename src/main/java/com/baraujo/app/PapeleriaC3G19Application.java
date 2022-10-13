606b69da1be09f210600eb662de4e96819f72266
BernardoAraujoC <bernardo.araujo@o365.unab.edu.co>
2022-10-12 20:43:26 -0500

    Se agregan los archivos necesarios para el proyecto

---
+++ src/main/java/com/baraujo/app/PapeleriaC3G19Application.java
@@ -0,0 +1,13 @@
+package com.baraujo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapeleriaC3G19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapeleriaC3G19Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/baraujo/app/PapeleriaC3G19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baraujo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapeleriaC3G19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
